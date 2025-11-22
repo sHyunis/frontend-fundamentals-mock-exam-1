d0e44c76554e2279f30a0202070f4d2830e0a093
sHyunis <162306191+sHyunis@users.noreply.github.com>
2025-11-22 14:28:25 +0900

    feat:header UI 를 분리한다.

---
+++ src/features/savings/components/Header.tsx
@@ -0,0 +1,10 @@
+import { NavigationBar, Spacing } from 'tosslib';
+
+export function Header() {
+  return (
+    <>
+      <NavigationBar title="적금 계산기" />
+      <Spacing size={16} />
+    </>
+  );
+}
