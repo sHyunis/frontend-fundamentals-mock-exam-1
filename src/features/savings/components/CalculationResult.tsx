e1ceb7d46cde9388854150de5e41cab0c9b6255d
sHyunis <162306191+sHyunis@users.noreply.github.com>
2025-11-24 19:18:46 +0900

    refactor: utils 폴더를 shared 내부로 이동시킨다.

--- src/features/savings/components/CalculationResult.tsx
+++ src/features/savings/components/CalculationResult.tsx
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled';
 import { Border, Checkbox, colors, ListHeader, ListRow, Spacing } from 'tosslib';
 import type { SavingsProduct } from '../types';
-import { formatNumberWithCommas } from '../../../utils/format';
+import { formatNumberWithCommas } from '../../../shared/components/utils/format';
 import { getTopProductsByRate } from '../utils/filter';
 
 interface CalculationResultProps {
--- src/features/savings/components/ProductList.tsx
+++ src/features/savings/components/ProductList.tsx
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled';
 import { Checkbox, colors, ListRow } from 'tosslib';
 import type { SavingsProduct } from '../types';
-import { formatNumberWithCommas } from '../../../utils/format';
+import { formatNumberWithCommas } from '../../../shared/components/utils/format';
 
 interface ProductListProps {
   products: SavingsProduct[];
